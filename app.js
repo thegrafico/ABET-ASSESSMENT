db44969ec902296e2b06ef346190ee6b9815db02
Kemuel Suarez <kemuel.s21@gmail.com>
2019-08-12 21:48:57 -0400

    Creating kemuelbranch

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require("hbs");
-
 var indexRouter = require('./routes/index');
 // ===== Evaluation Section =====
 var evaluationRouter = require('./routes/evaluations/evaluation');
