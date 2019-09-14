f5a848ef39e32c9643758638e2955ee32bf8fff9
Kemuel Suarez <kemuel.s21@gmail.com>
2019-09-14 17:46:13 -0400

    Added Profile dropdown to the createUsers.

--- routes/users/createUsers.js
+++ routes/users/createUsers.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../../helpers/mysqlConnection").mysql_pool;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('users/createUsers', { title: 'ABET Assessment' });
+
+  var parms = { title: 'ABET Assessment' };
+
+  db.getConnection (function (err, connection){
+
+    let userList = `Select *
+                    From PROFILE`
+
+    connection.query (userList,function (err,results,fields){
+
+      parms.profile = results;
+
+      res.render('users/createUsers', parms);
+    })
+    connection.release();
+  })
+
 });
 
 module.exports = router;
