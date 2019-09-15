2d250b063792ff7beb5802d110fc35c39be8cf09
Raul Pichardo <raul022107@gmail.com>
2019-09-15 11:45:43 -0400

    Added rountes and env

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ var authorize = require('./routes/authorize');
 
 var app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
