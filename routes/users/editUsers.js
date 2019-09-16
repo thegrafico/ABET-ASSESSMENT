dc1b0be3338ce9e14ebf5369e708302aa2622dd2
Raul Pichardo <raul022107@gmail.com>
2019-09-15 23:53:42 -0400

    Added userid for form

--- routes/users/editUsers.js
+++ routes/users/editUsers.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
+
+  let user_id = req.params.id;
+
+  console.log(user_id)
 
   var parms = { title: 'ABET Assessment' };
 
-
+  parms["user_id"] = user_id
 
   res.render('users/editUsers', parms );
 });
