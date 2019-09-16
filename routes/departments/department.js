50ce6dd91fbd78e091f58cddce1ef52b83fa8f3a
Carlos Alvarado <cjclonjedi@gmail.com>
2019-09-16 00:45:36 -0400

    Moved everything to one js with multiple routes. (Refactoring Code)

--- routes/departments/department.js
+++ routes/departments/department.js
@@ -2,10 +2,22 @@ var express = require('express');
 var router = express.Router();
 var db = require("../../helpers/mysqlConnection").mysql_pool;
 
+let base_url = '/department/'
+let routes_names = ['create', 'delete', 'edit', 'details']
+
+//Paramns to routes links
+let parms = {};
+
+//Populate parms
+routes_names.forEach(e=>{
+  parms[e] = base_url + e;
+});
+
+parms["title"] = 'ABET Assessment';
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  var parms = { title: 'ABET Assessment' };
 
   db.getConnection (function (err, connection){
 
@@ -22,7 +34,46 @@ router.get('/', function(req, res, next) {
     })
     connection.release();
   })
-
 });
 
-    module.exports = router;
+/* create Department home page. */
+router.get('/' + routes_names[0], function(req, res, next) {
+  res.render('departments/createDepartment', parms);
+});
+
+router.post('/' + routes_names[0], function(req, res, next) {
+  var depName = req.body.depName;
+  var depDesc = req.body.depDesc;
+
+  console.log(req.body.depName);
+
+  db.getConnection (function (err, connection){
+
+    let addDep = `insert into DEPARTMENT (dep_name, dep_description)
+    values( '${depName}', '${depDesc}');`
+
+    connection.query (addDep,function (err,results,fields){
+    res.render('departments/createDepartment', parms);
+    });
+    connection.release();
+  });
+});
+
+/* DELETE home page. */
+router.get('/' + routes_names[1], function(req, res, next) {
+  res.render('departments/deleteDepartment', parms);
+});
+
+/* EDIT home page. */
+router.get('/' + routes_names[2], function(req, res, next) {
+  res.render('departments/editDepartment', parms);
+});
+
+/* DETAILS home page. */
+router.get('/' + routes_names[3], function(req, res, next) {
+  res.render('departments/detailDepartment', parms);
+});
+
+
+
+module.exports = router;
