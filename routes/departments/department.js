9d82b6c7d93645c46769418eee4d4b90a73d94b9
Kemuel Suarez <kemuel.s21@gmail.com>
2019-09-15 21:44:51 -0400

    Displays real data from the database.

--- routes/departments/department.js
+++ routes/departments/department.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../../helpers/mysqlConnection").mysql_pool;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('departments/department', { title: 'ABET Assessment' });
+
+  var parms = { title: 'ABET Assessment' };
+
+  db.getConnection (function (err, connection){
+
+    let courseList = `Select *
+                      From DEPARTMENT`
+
+    connection.query (courseList,function (err,results,fields){
+
+      // console.log(results);
+      parms.results = results;
+
+      res.render('departments/department', parms);
+
+    })
+    connection.release();
+  })
+
 });
 
-module.exports = router;
+    module.exports = router;
