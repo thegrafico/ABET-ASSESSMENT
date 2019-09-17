b82954925125a45fe5ffb8a80363fb2bf5c6f21b
Raul Pichardo <raul022107@gmail.com>
2019-09-17 09:58:31 -0400

    Refactoring user route

--- routes/users/users.js
+++ routes/users/users.js
@@ -9,136 +9,118 @@ var parms = {
   title: 'ABET Assessment'
 };
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+/* USER HOME*/
+router.get('/', function (req, res) {
   try {
+
+    //Get all user from the database (callback)
     queries.get_user_list(function(err, results){
+      
       //TODO: redirect user to another page
-      if (err) throw err;
+      if (err){
+        //HERE HAS TO REDIRECT the user or send a error message 
+        throw err;
+      } 
 
-      console.log(results);
-
+      //IF found results from the database
       if (results){
-        // console.log(results)
+        console.log(results)
         parms.results = results;
       }
+
       res.render('users/users', parms);
     });
   } catch (error) {
+    //TODO: send a error message to the user. 
     console.log(error);
     res.redirect('/');
   }
 });
+/*
+EDIT USER ROUTE
+*/
+router.get('/:id/edit', function(req, res){
+  console.log("EDIT ROUTE");
 
+  var parms = { title: 'ABET Assessment' };
+  
+  //TODO: catch error in case there is not id
+  let user_id = req.params.id;
+  
+  queries.get_user_by_id(user_id, function(err, results){
+    parms.interID = results[0].inter_ID;
+    parms.fName = results[0].first_name;
+    parms.lName = results[0].last_name;
+    parms.email = results[0].email;
+    parms.pNumber = results[0].phone_number;
+    parms.userID = req.params.id;
+    // console.log(req.body.editButton);
+    res.render('users/editUsers', parms);
+  });
+});
+/*
+UPDATE USER ROUTE
+*/
+router.put('/:id', function(req, res){
+  console.log("================UPDATE ROUTE==================");
 
-router.post ('/', function (req, res){
-    var parms = { title: 'ABET Assessment' };
+  let user_data_to_update = req.body.data;
 
-    if (req.body.editButton != undefined){
-      db.getConnection (function (err, connection){
-
-        let getUser = `Select *
-                      FROM USER
-                      where user_ID= ${req.body.editButton}`
-
-          connection.query (getUser,function (err,results,fields){
-
-            console.log(results);
-
-            parms.interID = results[0].inter_ID;
-            parms.fName = results[0].first_name;
-            parms.lName = results[0].last_name;
-            parms.email = results[0].email;
-            parms.pNumber = results[0].phone_number;
-            parms.userID = req.body.editButton;
-            console.log(req.body.editButton);
-
-            res.render('users/editUsers', parms);
-
-          })
-        connection.release();
-      })
-    }
-    else if (req.body.finalEditButton != undefined){
-
-      db.getConnection (function (err, connection){
-
-        var interID = req.body.interID;
-        var fName   = req.body.fName;
-        var lName   = req.body.lName;
-        var email   = req.body.email;
-        var pNumber = req.body.pNumber;
-        console.log(req.body.finalEditButton);
-
-        let updateUser = `update USER
-                         set inter_ID= '${interID}', first_name= '${fName}',
-                         last_name= '${lName}', email= '${email}', phone_number= '${pNumber}'
-                         where user_ID= ${req.body.finalEditButton}`
-
-        connection.query (updateUser,function (err,results,fields){
-        })
-        res.redirect('/users');
-        connection.release();
-      })
-    }
-    else if (req.body.deleteButton != undefined){
-
-      db.getConnection (function (err, connection){
-
-        let findUser = `Select *
-                        From USER
-                        where user_ID = ${req.body.deleteButton}`
-
-        connection.query (findUser,function (err,results,fields){
-
-          console.log(results);
-
-          parms.userID = results[0].user_ID;
-          parms.interID = results[0].inter_ID;
-          parms.fName = results[0].first_name;
-          parms.lName = results[0].last_name;
-          parms.email = results[0].email;
-          parms.pNumber = results[0].phone_number;
-          parms.date = results[0].date_created;
-
-          res.render('users/deleteUsers', parms);
-        })
-        connection.release();
-      })
-    }
-    else if(req.body.finalDeletebutton != undefined){
-
-    let findUser = `Select *
-                    From USER
-                    where user_ID = ${req.body.deleteButton}`;
-
-    conn.query(findUser, function (err, results, fields) {
+  queries.update_user(user_data_to_update, function(err, results){
+    if (err){
       //TODO: catch error
-      // console.log(results);
+      throw err;
+    }    
+    console.log("User update")
+    res.redirect("/users");
+  });
 
-      parms.userID = results[0].user_ID;
-      parms.interID = results[0].inter_ID;
-      parms.fName = results[0].first_name;
-      parms.lName = results[0].last_name;
-      parms.email = results[0].email;
-      parms.pNumber = results[0].phone_number;
-      parms.date = results[0].date_created;
+  console.log("================UPDATE ROUTE==================");
+});
 
-      res.render('users/deleteUsers', parms);
-    });
-      
-  }else if (req.body.finalDeletebutton != undefined) {
 
-    let deleteUser = `DELETE
-                      FROM USER
-                      WHERE user_ID = ${req.body.finalDeletebutton};`;
-                  
-    conn.query(deleteUser, function (err, results, fields) {});
+/*
+REMOVE USER ROUTE
+*/
 
-    res.redirect('/users');
-  }else {
-      res.redirect('/users');
+router.get('/:id/remove', function(req, res){
+  console.log("REMOVE ROUTE");
+
+  var parms = { title: 'ABET Assessment' };
+  
+  //TODO: catch error in case there is not id
+  let user_id = req.params.id;
+  
+  queries.get_user_by_id(user_id, function(err, results){
+    parms.interID = results[0].inter_ID;
+    parms.fName = results[0].first_name;
+    parms.lName = results[0].last_name;
+    parms.email = results[0].email;
+    parms.pNumber = results[0].phone_number;
+    parms.userID = req.params.id;
+    res.render('users/deleteUsers', parms);
+  });
+});
+
+/*
+DELETE ROUTE
+*/ 
+router.delete('/:id', function(req, res){
+  console.log("===================DELETED ROUTE=====================");
+  
+  //TODO: catch error in case of null
+  let user_id = req.params.id;
+
+  queries.delete_user_by_id(user_id, function(err, results){
+    
+    //TODO: catch error
+    if (err){
+      throw err;
     }
+    console.log("USER DELETED")
+    console.log("===================DELETED ROUTE=====================");
+    res.redirect("/users");
+  });
 });
 
 module.exports = router;
