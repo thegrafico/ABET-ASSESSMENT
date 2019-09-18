d7498b7c92523af671971a0b4f393c62eae965f6
Raul Pichardo <raul022107@gmail.com>
2019-09-17 21:10:37 -0400

    Editing deparment

--- routes/departments/department.js
+++ routes/departments/department.js
@@ -1,7 +1,10 @@
+//Variables and queries to use
 var express = require('express');
 var router = express.Router();
-var db = require("../../helpers/mysqlConnection").mysql_pool;
+var query = require("../../helpers/queries/department_queries");
+var general_queries = require("../../helpers/queries/general_queries");
 
+//Routing for CRUD
 let base_url = '/department/'
 let routes_names = ['create', 'delete', 'edit', 'details']
 
@@ -9,126 +12,139 @@ let routes_names = ['create', 'delete', 'edit', 'details']
 let parms = {};
 
 //Populate parms
-routes_names.forEach(e=>{
-  parms[e] = base_url + e;
+routes_names.forEach(e => {
+	parms[e] = base_url + e;
 });
-
+//Same title for every department route??
 parms["title"] = 'ABET Assessment';
+//============================================ DEPARMENT MAIN ROUTE =============================
 
+/* DEPARTMENT home page. */
+router.get('/', function (req, res, next) {
+	
+	//Table to look into the database
+	let deparment_table = "DEPARTMENT";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+	//Getting the information from db
+	general_queries.get_table_info(deparment_table, function (err, results) {
+		
+		//TODO: redirect user to another route or send messsage to the user
+		if(err) throw err;
 
-  db.getConnection (function (err, connection){
+		// console.log("DEBUG: ", results)
+		parms.results = results;
+		
+		res.render('departments/department', parms);
+	});
 
-    let courseList = `Select *
-                      From DEPARTMENT`
+});
+//===================================CREATE DEPARTMENT=====================================
 
-    connection.query (courseList,function (err,results,fields){
-
-      // console.log(results);
-      parms.results = results;
-
-      res.render('departments/department', parms);
-
-    })
-    connection.release();
-  })
+// GET
+router.get('/' + routes_names[0], function (req, res) {	
+		res.render('departments/createDepartment', parms);
 });
 
-/* create Department home page. */
-router.get('/' + routes_names[0], function(req, res, next) {
-  res.render('departments/createDepartment', parms);
+// POST
+router.post('/' + routes_names[0], function (req, res, next) {
+	
+	//TODO: verify is empty
+	var depName = req.body.depName;
+	var depDesc = req.body.depDesc;
+
+	//Insert into the DB the data from user
+	query.insert_into_deparment([depName, depDesc], function(err, results){
+
+		//TODO: catch error properly
+		if (err) throw err;
+
+		console.log("DEPARTMENT INSERTED WITH THE ID: ", results.insertId);
+
+		res.redirect(base_url);
+	});
+});
+//===================================DELETE DEPARTMENT=====================================
+
+// GET
+router.get('/:id/' + routes_names[1], function (req, res, next) {
+
+
+	let tabla_data = {"table_name": "DEPARTMENT", "atribute": "dep_ID", "id": req.params.id };
+
+	general_queries.get_table_info_by_id(tabla_data, function (err, results) {
+		
+		//TODO: catch error
+		if (err) throw err;
+		
+		parms.dep_ID = results[0].dep_ID;
+		parms.depName = results[0].dep_name;
+		parms.depDesc = results[0].dep_description;
+		parms.depDate = results[0].date_created;
+
+		res.render('departments/deleteDepartment', parms);
+	});
+
 });
 
-router.post('/' + routes_names[0], function(req, res, next) {
-  var depName = req.body.depName;
-  var depDesc = req.body.depDesc;
+// DELETE
+router.delete('/:id' , function (req, res, next) {
 
-  console.log(req.body.depName);
+	let tabla_data = {"table_name": "DEPARTMENT", "atribute": "dep_ID", "id": req.params.id };
 
-  db.getConnection (function (err, connection){
+	general_queries.delete_record_by_id(tabla_data, function(err, results){
+		//TODO: catch error
+		if (err) throw err;
 
-    let addDep = `insert into DEPARTMENT (dep_name, dep_description)
-                  values( '${depName}', '${depDesc}');`
+		console.log("Record delete");
+		res.redirect(base_url);
+	});
+});
+//================================== EDIT ROUTE=============================================
 
-    connection.query (addDep,function (err,results,fields){
-    res.render('departments/createDepartment', parms);
-    });
-    connection.release();
-  });
+/* GET */
+router.get('/:id/' + routes_names[2], function (req, res, next) {
+
+	let tabla_data = {"table_name": "DEPARTMENT", "atribute": "dep_ID", "id": req.params.id };
+
+	general_queries.get_table_info_by_id(tabla_data, function (err, results) {
+		
+		//TODO: catch error
+		if (err) throw err;
+		
+		parms.dep_ID = results[0].dep_ID;
+		parms.depName = results[0].dep_name;
+		parms.depDesc = results[0].dep_description;
+
+		res.render('departments/editDepartment', parms);
+	});
+
 });
 
-/* DELETE home page. */
-router.get('/:id/' + routes_names[1], function(req, res, next) {
+/* EDIT */
+router.put('/:id', function (req, res, next) {
 
-  let findDep = `Select *
-                From DEPARTMENT
-                where dep_ID = ${req.params.id};`
+	//TODO: validate user data
+	let dpt_data = [req.body.depName, req.body.depDesc, req.params.id] 
+	
+	//Exe the query into the database
+	query.update_deparment(dpt_data, function(err, results){
+	
+		//TODO: catch error
+		if (err){
+			throw err;
+		}
 
-  db.getConnection (function (err, connection){
-    connection.query (findDep,function (err,results,fields){
-      parms.dep_ID = results[0].dep_ID;
-      parms.depName = results[0].dep_name;
-      parms.depDesc = results[0].dep_description;
-      parms.depDate = results[0].date_created;
-
-      res.render('departments/deleteDepartment', parms);
-    });
-    connection.release();
-  });
+		console.log("USER EDITED!")
+		res.redirect(base_url);
+	});
 });
 
-router.post('/:id/' + routes_names[2], function(req, res, next) {
+//===============================================================================
 
-  let deleteDep = `DELETE
-                  FROM DEPARTMENT
-                  WHERE dep_ID = ${req.params.id};`
-
-  db.getConnection (function (err, connection){
-    connection.query (deleteDep,function (err,results,fields){
-    });
-    res.redirect('/department');
-    connection.release();
-  });
-});
-
-/* EDIT home page. */
-router.get('/:id/' + routes_names[2], function(req, res, next) {
-
-  let findDep = `Select *
-                From DEPARTMENT
-                where dep_ID = ${req.params.id};`
-
-  db.getConnection (function (err, connection){
-    connection.query (findDep,function (err,results,fields){
-      parms.dep_ID = results[0].dep_ID;
-      parms.depName = results[0].dep_name;
-      parms.depDesc = results[0].dep_description;
-
-      res.render('departments/editDepartment', parms);
-    });
-    connection.release();
-  });
-});
-
-router.post('/:id/' + routes_names[2], function(req, res, next) {
-
-  let updateDep = `update DEPARTMENT
-                   set dep_name= '${req.body.depName}', dep_description= '${req.body.depDesc}'
-                   where dep_ID= ${req.params.id}`
-
-  db.getConnection (function (err, connection){
-    connection.query (updateDep,function (err,results,fields){
-    });
-    res.redirect('/department');
-    connection.release();
-  });
-});
 
 /* DETAILS home page. */
-router.get('/' + routes_names[3], function(req, res, next) {
-  res.render('departments/detailDepartment', parms);
+router.get('/' + routes_names[3], function (req, res, next) {
+	res.render('departments/detailDepartment', parms);
 });
 
 
