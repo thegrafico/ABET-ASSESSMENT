81347087e6ab46c8263f452d1404148e384dc194
Carlos Alvarado <cjclonjedi@gmail.com>
2019-09-23 15:30:26 -0400

    Added the backend to display the real data.

--- routes/evaluations/evaluation.js
+++ routes/evaluations/evaluation.js
@@ -3,9 +3,11 @@
 
 var express = require('express');
 var router = express.Router();
+var queries = require('../../helpers/queries/evaluation_queries');
+var general_queries = require('../../helpers/queries/general_queries');
 // var queries = require('../helpers/queries');
 
-let base_url = '/evaluation/' 
+let base_url = '/evaluation/'
 let routes_names = ['create', 'delete', 'edit']
 
 //Paramns to routes links
@@ -18,19 +20,60 @@ routes_names.forEach(e=>{
 
 // console.log("ROUTES", parms, "FINISH")
 
-parms["title"] = 'ABET Assessment'; 
+parms["title"] = 'ABET Assessment';
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('evaluations/evaluation', parms);
+  try{
+
+    let evaluation_table = 'EVALUATION_RUBRIC';
+
+    //Get all perfCrit from the database (callback)
+    general_queries.get_table_info(evaluation_table, function (err, results) {
+
+      //TODO: redirect user to another page
+      if (err) {
+        //HERE HAS TO REDIRECT the user or send a error message
+        throw err;
+      }
+
+      //IF found results from the database
+      if (results) {
+        // console.log(results)
+        parms.results = results;
+      }
+
+      res.render('evaluations/evaluation', parms);
+    });
+  }
+  catch (error) {
+
+    //TODO: send a error message to the user.
+    console.log(error);
+    res.render('evaluations/evaluation', parms);
+  }
 });
 
 /* Create home page. */
 router.get('/' + routes_names[0], function(req, res, next) {
-  
-  res.render('evaluations/createEvaluation', parms);
+  try{
+
+
+
+
+
+
+    res.render('evaluations/createEvaluation', parms);
+  }
+  catch (error) {
+    //TODO: send a error message to the user.
+    console.log(error);
+    res.render('evaluations/createEvaluation', parms);
+  }
 });
+
+
 
 
 /* DELETE home page. */
