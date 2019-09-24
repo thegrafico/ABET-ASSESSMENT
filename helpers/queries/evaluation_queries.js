b6ce8babcdffe6f7143e6236d70c8003d3b3b988
Carlos Alvarado <calvarado1458@interbayamon.edu>
2019-09-24 04:04:43 -0400

    Added Update Query

--- helpers/queries/evaluation_queries.js
+++ helpers/queries/evaluation_queries.js
@@ -2,7 +2,7 @@ var conn = require("../mysqlConnection").mysql_pool; //pool connection
 
 
 function insert_evalRub(data, callback) {
-    `Insert values into the table department`
+    // `Insert values into the table department`
     // outc_ID, outc_name, outc_description, date_created, prog_ID
 
     let insert_query = `insert into EVALUATION_RUBRIC (rubric_name, rubric_description, outc_ID) values(?, ?, ?);`;
@@ -16,4 +16,22 @@ function insert_evalRub(data, callback) {
     });
 }
 
+function update_evalRub(data, callback) {
+    // `Insert values into the table department`
+    // outc_ID, outc_name, outc_description, date_created, prog_ID
+    console.log(data);
+    let update_query = `update EVALUATION_RUBRIC
+                        set rubric_name = ?, rubric_description = ?, outc_ID = ?
+                        where rubric_ID = ?`;
+
+    conn.query(update_query, data, function (err, results, fields) {
+        if (err) {
+            return callback(err, null)
+        };
+        // console.log(results)
+        return callback(null, results);
+    });
+}
+
 module.exports.insert_evalRub = insert_evalRub;
+module.exports.update_evalRub = update_evalRub;
