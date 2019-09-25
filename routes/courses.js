e2290ab5626d093f116d705eeef63a3eb93d0222
Kemuel Suarez <kemuel.s21@gmail.com>
2019-09-24 20:08:18 -0400

    Edit feature added

--- routes/courses.js
+++ routes/courses.js
@@ -22,7 +22,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-//================================ CREATE STUDY PROGRAM  =================================
+//================================ CREATE COURSE  =================================
 /* CREATE home page. */
 router.get('/create', function(req, res, next) {
   let deparment_table = "STUDY_PROGRAM";
@@ -38,27 +38,124 @@ router.get('/create', function(req, res, next) {
 
 router.post('/create', function(req, res, next) {
 
-  // dep_ID, dep_name, dep_description
-
 
   //TODO: verify values, null, undefined
   let data = req.body.data;
 
 	//Insert into the DB the data from user
-	query.insert_into_course([data.crnumber,data.crname, dta.crdesc, data.prog_id], function(err, results){
-
+	query.insert_into_course([data.crnumber,data.crname, data.crdesc, data.prog_id], function(err, results){
 		//TODO: catch error properly
+    // console.log("HERE", prog_id);
 		if (err) throw err;
 
-		// console.log("STUDY PROGRAMN INSERTED WITH THE ID: ", results.insertId);
 
 		res.redirect(base_url);
+
+
+
 	});
 });
 
+//==================================== REMOVE STUDY PROGRAM ROUTE =================================
+/* REMOVE STUDY PROGRAM ROUTE */
+router.get('/:id/remove', function (req, res) {
+  console.log("REMOVE ROUTE");
 
+  //TODO: catch error in case there is not id
+  let course_id = req.params.id;
+  let course_table = "COURSE";
+  let where_attr = "course_ID";
+  let data = {"from": course_table,"where":where_attr, "id":course_id};
+  general_queries.get_table_info_by_id(data, function(err, results){
 
+    //TODO: catch erro
+    if (err) throw err;
+    try {
+      parms.course_ID = results[0].course_ID;
+      parms.course_name = results[0].course_name;
+      parms.course_description = results[0].course_description;
+      parms.date_created = results[0].date_created;
+      res.render('courses/deleteCourses', parms);
+    } catch (error) {
+      res.redirect(base_url);
+    }
+  });
+});
+/* DELETE ROUTE */
+router.delete('/:id', function (req, res) {
+  console.log("===================DELETED ROUTE=====================");
 
+  //TODO: catch error in case of null
+  let course_id = req.params.id;
+  let table_name = "COURSE";
+  let where_attr = "course_ID";
+
+  let data = {"id":course_id, "from":table_name,"where":where_attr };
+
+  general_queries.delete_record_by_id(data, function (err, results) {
+
+    //TODO: catch error
+    if (err) {
+      throw err;
+    }
+    console.log("===================DELETED ROUTE=====================");
+    res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    res.redirect("back");
+  });
+});
+
+//========================================== EDIT ROUTE =====================================
+/* EDIT home page. */
+router.get('/:id/edit', function(req, res, next) {
+  let table_name = "COURSE";
+  let id_course = req.params.id;
+  let where_atr = "course_ID";
+
+  let data = {"from":table_name, "where":where_atr, "id": id_course};
+  general_queries.get_table_info_by_id(data, function(err, user_results){
+
+    //TODO: handle this err;
+    if(err)throw err;
+
+    let prog_table = "STUDY_PROGRAM";
+
+    general_queries.get_table_info(prog_table, function(error, resutls){
+
+      //TODO: handle this err;
+      if(error)throw error;
+
+      parms.course_ID = id_course;
+      parms.std_results = resutls;
+      parms.user_results = user_results[0];
+
+      // console.log("EDIT RESULTS: ", parms);
+      res.render('courses/editCourses', parms);
+    });
+  });
+});
+
+/* EDIT home page. */
+router.put('/:id', function(req, res, next) {
+
+  //TODO: verify values befero enter to DB
+  let name = req.body.data.crname;
+  let course_id = req.params.course_id;
+  let prog_id =  req.body.data.prog_id;
+  let course_desc = req.body.data.crdesc;
+  let course_number = req.body.data.crnumber;
+
+  //TIENE QUE IR EN ESTE ORDEN.
+  let data = [name, prog_id, course_desc, course_number, course_id];
+
+  query.update_course(data, function(err, results){
+
+    //TODO: cath this error
+    if (err) throw err;
+
+    console.log("EDITED STUDY PROGRAM");
+    res.redirect(base_url);
+  });
+});
 
 
 module.exports = router;
