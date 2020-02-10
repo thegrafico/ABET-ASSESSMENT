1b8f63e6aba908beff953cb6eaad2f4a54e202da
NoahRAlmeda <noahalmeda@gmail.com>
2020-02-10 14:26:49 -0400

    Created a file to hold report genarator function. And switch perfTablequeries from callback to promise

--- helpers/queries/perfTable_queries.js
+++ helpers/queries/perfTable_queries.js
@@ -3,11 +3,13 @@ var conn = db.mysql_pool;
 
  /***
   * Return perfomance criterias being evaluated for an Assessment report
-  * Parameters is the assessment ID
+  * @param {Number} assessmentID -> Assessment ID
+  * @returns {Promise} -> Resolves contain query results.
  */
 
-function get_perf_criterias(assessmentID ,callback){
-  let findPerfCrit = `SELECT perC_Desk
+function get_perf_criterias(assessmentID){
+	return new Promise(function(resolve, reject) {
+		let findPerfCrit = `SELECT perC_Desk
                       FROM PERF_CRITERIA, (SELECT perC_ID
                                            FROM PERFORMANCE_RUBRIC,(SELECT rubric_ID
                                                                     FROM EVALUATION_RUBRIC
@@ -19,39 +21,33 @@ function get_perf_criterias(assessmentID ,callback){
                                                                   ) as result
                                            WHERE PERFORMANCE_RUBRIC.rubric_ID = result.rubric_ID
                                            ) as resultTwo
-                      WHERE PERF_CRITERIA.perC_ID = resultTwo.perC_ID`;
-  try {
-      conn.query(findPerfCrit,[assessmentID] ,function (err, results) {
-
-          if (err) {
-              return callback(err, null)
-          };
-          return callback(null, results);
-      });
-  } catch (error) {
-      // console.log("ERROR IN get_table_info_id");
-      return callback(error, null);
-  }
+					  WHERE PERF_CRITERIA.perC_ID = resultTwo.perC_ID`;
+			conn.query(findPerfCrit, [assessmentID], function (err, results) {
+			if (err) 
+				reject(err || "Error retreiving performance criterias.");
+			else
+				resolve(results);
+		});
+	});
 }
 
 /***
- * Post Student performance criterias into database
+ * Insert students evaluation results to STUDENT_PERFORMANCE table
+ * @param {Array} data -> Array that contains each of the perfomance criterias score and the assessment ID
+ * @returns {Promise} -> Returns true if query was able to insert the data
 */
 
-function insertData(data, callback) {
-    let insertData = `INSERT INTO STUDENT_PERFORMANCE(pc_1, pc_2, pc_3, pc_4, pc_5, assessment_ID) VALUES(?, ?, ?, ?, ?, ?)`;
-    try {
-        conn.query(insertData,data ,function (err, results) {
-  
-            if (err) {
-                return callback(err, null)
-            };
-            return callback(null, results);
+function insertData(data) {
+	return new Promise(function(resolve, reject){
+		let insertData = `INSERT INTO STUDENT_PERFORMANCE(pc_1, pc_2, pc_3, pc_4, pc_5, assessment_ID) VALUES(?, ?, ?, ?, ?, ?)`;
+		conn.query(insertData,data ,function (err, results) {
+            if (err)
+                reject(err || "Wasn't able to insert data");
+            else
+            	resolve(true);
         });
-    } catch (error) {
-        return callback(error, null);
-    }
-};
+	});
+}
 
 module.exports.get_perf_criterias = get_perf_criterias;
 module.exports.insertData = insertData;
